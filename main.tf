dd93a464c6eadc20f13cb11277d92fbab1ef2374
Pankhuri22-P <pankhurip30@gmail.com>
2026-01-07 17:05:40 +0100

    Added lambda permission to invoke url

--- main.tf
+++ main.tf
@@ -25,6 +25,23 @@ resource "aws_lambda_function_url" "slack2opsgenie_functionurl" {
   function_name      = module.lambda_slack2opsgenie.lambda_function_name
   authorization_type = "NONE"
 }
+
+resource "aws_lambda_permission" "allow_invoke" {
+  statement_id  = "AllowPublicInvoke"  
+  action        = "lambda:InvokeFunction"
+  principal     = "*"
+  function_name = module.lambda_slack2opsgenie.lambda_function_name  
+  qualifier     = "$LATEST"  
+}
+
+resource "aws_lambda_permission" "allow_invoke_function_url" {
+  statement_id  = "AllowPublicInvokeFunctionUrl"
+  action        = "lambda:InvokeFunctionUrl"
+  principal     = "*"
+  function_name = module.lambda_slack2opsgenie.lambda_function_name
+  qualifier     = "$LATEST"  
+}  
+
 #creates a iam role for lambda that sends the message with proper keyword in slack channel to opsgenie to create an alert 
 module "iam_role_slack2opsgenie" {
   source    = "github.com/wearetechnative/terraform-aws-iam-role.git?ref=9229bbd0280807cbc49f194ff6d2741265dc108a"
